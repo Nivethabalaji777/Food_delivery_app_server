4f4b2c5cddbddb6620c3fdc4939fa0e761c95564
Nivetha T <127500611+Nivethabalaji777@users.noreply.github.com>
2023-11-23 12:19:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ app.use(express.json());
 //     optionSuccessStatus:200
 //   })
 // );
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', `${process.env.CLIENT_URL}`);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+app.use((req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
 });
 // --------------------
 const Pizza = require("./Models/Food_Items");
